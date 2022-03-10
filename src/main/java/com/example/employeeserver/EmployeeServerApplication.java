aa35d13dd7c97dfbe71ce4724ba357afec75c4e5
ibmcloudmicro18 <shubhamsundriyal20@gmail.com>
2022-03-10 18:27:50 +0530

    1 Commit

---
+++ src/main/java/com/example/employeeserver/EmployeeServerApplication.java
@@ -0,0 +1,15 @@
+package com.example.employeeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EmployeeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeServerApplication.class, args);
+	}
+
+}
